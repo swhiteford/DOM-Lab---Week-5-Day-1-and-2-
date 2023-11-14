fcec99b24c18c96de0ec94ee6cb90178ee2f4723
Susan Whiteford <swhiteford1218@gmail.com>
2023-11-13 20:34:01 -0500

    Week 5 Day 1 & 2 Lab

---
+++ script.js
@@ -0,0 +1,11 @@
+const mainEl = document.querySelector ('main') 
+
+ mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
+ 
+ mainEl.classList.add ('flex-ctr')
+
+ const topMenuEl = document.querySelector ('#top-menu')
+
+ topMenuEl.style.height = '100%'
+
+ 
